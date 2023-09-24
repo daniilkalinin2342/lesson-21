74aa24433ef7025699bb1f3d0313cd89df84c2a4
daniilkalinin2011 <provenym1245@gmail.com>
2023-09-24 11:45:32 +0300

    commit

---
+++ main.js
@@ -0,0 +1,122 @@
+// console.log(4 > 9);
+// console.log(4 < 9);
+// console.log(22 === 22);
+// console.log(17 != 22);
+// console.log(22 >= 22);
+// console.log(12 <= 6);
+
+
+
+// let num = 18;
+
+// if (num > 10 && num < 20) {
+//     console.log("Текст тест");
+// }
+
+// let num1 = 8;
+
+// if (num1 > 10 || num1 < 2) {
+//     console.log("Текст тест");
+// }
+
+// let userName = "Саша";
+
+// if (userName == "Настя") {
+//     console.log("Привет, Настя");
+// } else if (userName == "Света") {
+//     console.log("Привет, Света");
+// } else if (userName == "Егор") {
+//     console.log("Привет, Егор");
+// } else if (userName == "Саша") {
+//     console.log("Привет, Саша");
+// } else {
+//     console.log("Кто ты, Воин")
+// }
+
+
+// let userName = "Боб"
+
+// switch (userName) {
+//     case "Антон":
+//         console.log("Привет, Антон");
+//         break;
+//     case "Боб":
+//         console.log("Привет, Боб");
+//         break;
+//     case "Вася":
+//         console.log("Привет, Вася");
+//         break;
+//     default:
+//         console.log("Кто ты, Воин");
+//         break;
+// }
+
+
+// let mark = 22;
+
+// switch (mark) {
+//     case 2:
+//     case 3:
+//         console.log('Вы получили плохую оценку');
+//         break;
+
+//     case 4:
+//     case 5:
+//         console.log('Вы получили хорошую оценку');
+//         break;
+
+//     default:
+//         console.log('Ты не будешь играть на PS-5');
+//         break;
+// }
+
+// let num = 0
+// switch (num) {
+//     case 0:
+//         console.log("Это число 0");
+//         break;
+//     case 1:
+//         console.log("Это число 1");
+//         break;
+//     case 2:
+//     case 3:
+//         console.log("Это число 2 или 3");
+//         break;
+//     default:
+//         console.log("Неизвестное число");
+//         break;
+// }
+
+
+// let mark = 3;
+
+// if (mark === 2 || mark === 3) {
+//     console.log("Вы получили плохую оценку");
+// } else if (mark == 4 || mark === 5) {
+//     console.log("Вы получили хорошую оценку");
+// } else {
+//     console.log("Кажется кто то останется без игры");
+// }
+
+// let number = '13';
+// console.log(typeof(number));
+
+// let number1 = 189;
+// console.log(typeof(number1));
+
+// let number = 321;
+// console.log(number.toString());
+
+// let num = '12'
+// num = parseInt(num)
+// console.log(num);
+
+// let number = 'user'
+// number = parseInt(number);
+// console.log(typeof(number));
+// console.log(number)
+
+// let number1 = 'user';
+// let number2 = '212'
+// console.log(isNaN(number1));
+// console.log(isNaN(number2));
